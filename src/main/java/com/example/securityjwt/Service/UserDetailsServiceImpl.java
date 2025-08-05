d11aef2a93eebe361976579bb87ea59fc8294402
seoyeon <2371013@ewhain.net>
2025-08-05 16:37:22 +0900

    UserDetailsServiceImpl 주석 추가

--- src/main/java/com/example/securityjwt/Service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/securityjwt/Service/UserDetailsServiceImpl.java
@@ -10,14 +10,14 @@ import org.springframework.stereotype.Service;
 @Service  // Spring이 이 클래스를 Service 계층으로 인식하고 Bean으로 등록
 @RequiredArgsConstructor  // 생성자 주입 (final 필드를 자동으로 생성자 주입)
 public class UserDetailsServiceImpl implements UserDetailsService {
-
+    //UserDetailsService -> Spring Security에서 사용자 인증을 담당하는 핵심 인터페이스
+    //loadUserByUsername -> Security가 로그인 시 자동으로 호출하는 메서드
     private final UserRepository userRepository;  // DB에서 사용자 정보를 조회하는 Repository (JPA)
 
     /*
      Spring Security가 로그인 시 호출하는 메서드
      username (ID)로 사용자 정보를 조회하여 반환 -> UserDetails를 반환해야 Spring Security가 인증 처리를 할 수 있음
-     @param username 로그인 시 입력한 사용자명 (ID)
-     @return UserDetails (Spring Security가 이해할 수 있는 사용자 정보 객체)
+     UserDetails ->Spring Security가 이해할 수 있는 사용자 정보 객체
      @throws UsernameNotFoundException 사용자를 찾지 못했을 때 발생시키는 예외
      */
     @Override

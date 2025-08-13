096c28dbb403e8139b344f2655bd41f5d1ef2365
seoyeon <2371013@ewhain.net>
2025-08-13 15:34:51 +0900

    DTO 주석 수정

--- src/main/java/com/example/securityjwt/dto/LoginRequest.java
+++ src/main/java/com/example/securityjwt/dto/LoginRequest.java
@@ -8,6 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+// 소셜 로그인은 이 DTO를 사용하지 않음 -> OAuth2 인증을 통해 User 엔티티를 생성하고 JWT 발급
 public class LoginRequest {
     private String username;
     private String password;
--- src/main/java/com/example/securityjwt/dto/TokenResponse.java
+++ src/main/java/com/example/securityjwt/dto/TokenResponse.java
@@ -2,7 +2,7 @@ package com.example.securityjwt.dto;
 
 import lombok.*;
 
-// 로그인/재발급 응답 DTO -> 쿠키 전략이라면 바디에 토큰을 포함하지 않아도 되지만, 테스트/문서화 편의를 위해 유지하는 경우가 많다고함
+// 로그인/재발급 응답 DTO -> 쿠키 전략이라면 바디에 토큰을 포함하지 않아도 됌
 @Getter
 @Setter
 @NoArgsConstructor
@@ -10,5 +10,5 @@ import lombok.*;
 @Builder
 public class TokenResponse {
     private String accessToken;
-    private String refreshToken; // 쿠키만 쓸 땐 null로 내려도 무방
+    private String refreshToken;
 }
